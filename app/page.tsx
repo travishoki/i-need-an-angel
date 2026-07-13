58b4847a1ee747041db9bcf5fcdde27a8295cdef
Travis Hoki <travis.hoki@gmail.com>
2026-07-13 17:38:49 -0600

    loop through minion posters
    Just adding in 3 movie poster images

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+import Image from "next/image";
+
+const posters = [
+  {
+    src: "https://static.wikia.nocookie.net/despicableme/images/9/9c/Despicable_me_ver6_xlg.jpg/revision/latest/scale-to-width-down/267",
+    alt: "Despicable Me poster",
+    width: 267,
+    height: 395,
+  },
+  {
+    src: "https://static.wikia.nocookie.net/despicableme/images/6/6a/Gru.jpeg/revision/latest/scale-to-width-down/268?cb=20210119032211",
+    alt: "Gru",
+    width: 268,
+    height: 424,
+  },
+  {
+    src: "https://static.wikia.nocookie.net/despicableme/images/1/11/Minions_Poster_01.jpg/revision/latest",
+    alt: "Minions poster",
+    width: 1293,
+    height: 2048,
+  },
+];
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -10,7 +33,18 @@ export default function Home() {
             Good Code. Better Karma. Banana.
           </p>
         </div>
-
+        <div className="grid grid-cols-3 gap-4 w-full">
+          {posters.map((poster) => (
+            <Image
+              key={poster.src}
+              src={poster.src}
+              alt={poster.alt}
+              width={poster.width}
+              height={poster.height}
+              className="w-full h-64 object-cover rounded-lg"
+            />
+          ))}
+        </div>
       </main>
     </div>
   );
