b56f96f1768bd0e015102c323cea634a245ee50f
Travis Hoki <travis.hoki@gmail.com>
2026-07-14 01:45:10 -0600

    header branding is a home link

--- app/components/Header/Branding.tsx
+++ app/components/Header/Branding.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Branding() {
   return (
-    <div className="flex min-w-0 items-center gap-3 sm:gap-4">
+    <Link href="/" className="flex min-w-0 items-center gap-3 sm:gap-4">
       <Image
         src="https://i.pinimg.com/564x/42/03/2d/42032d0e52fed8b6a1293c74f35bfc0f.jpg"
         alt="Minion angel with wings and halo"
@@ -18,6 +19,6 @@ export default function Branding() {
           Good Code. Better Karma. Banana.
         </p>
       </div>
-    </div>
+    </Link>
   );
 }
