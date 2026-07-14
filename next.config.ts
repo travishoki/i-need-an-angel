434b37de7bcb6aef8b1b723dbc7b14bbc6a2bb2f
Travis Hoki <travis.hoki@gmail.com>
2026-07-14 01:22:11 -0600

    next white list external images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.wikia.nocookie.net",
+        port: "",
+        pathname: "/despicableme/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+        port: "",
+        pathname: "/564x/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
